2f2ead28bffab796a0553129d9f9541c92f11902
Filip Strajnar <filip.strajnar@gmail.com>
2023-06-04 01:20:29 +0200

    Changed parsing

--- data/src/data/__main__.py
+++ data/src/data/__main__.py
@@ -5,13 +5,13 @@ import re
 def readfile(path: str):
     with open(f"assets/{path}", "r") as f:
         try:
-            replaced, _ = re.subn(r"[^a-zA-Z0-9]", r" ", f.read())
+            replaced, _ = re.subn(r"[^a-zA-Z]", r" ", f.read())
             return replaced
         except Exception:
             return ""
 
 
-with open("data.txt", "w") as f:
+with open("input.txt", "w") as f:
     for path in os.listdir("assets"):
         try:
             f.write(f"{readfile(path)}\n")
