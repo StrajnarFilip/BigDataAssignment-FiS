dea5428a508e07fac36179b73620297c26516e47
Filip Strajnar <filip.strajnar@gmail.com>
2023-06-04 12:11:29 +0200

    Updated compose, made a simple build script

---
+++ build.ps1
@@ -0,0 +1,2 @@
+docker build --tag "988977i/fis-hadoop:two" .
+docker push "988977i/fis-hadoop:two"
--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
 export PATH="$PATH:/hadoop/hadoop-3.3.5/bin"
+# https://hadoop.apache.org/docs/current/hadoop-project-dist/hadoop-common/CommandsManual.html#jar
 hadoop "jar" $HADOOP_JAR_PATH $HADOOP_MAINCLASS $HADOOP_INPUT $HADOOP_OUTPUT
